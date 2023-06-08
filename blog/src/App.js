f28800685a3344e0ddc7e89bcd0125be253179d5
Forestsong22 <byu4n07@naver.com>
2023-06-08 16:20:02 +0900

    input !!!

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,10 +8,9 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', post, '파이썬 독학']);
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
-  [1,2,3].map(function(a){
-    return '123123123'
-  })
 
   return (  
     <div className="App">
@@ -29,49 +28,43 @@ function App() {
         copy[0] = '여자 코트 추천'
         글제목변경(copy);
         }}>수정버튼</button>
-      {/* <div className='list'>
-        <h4>{ 글제목[0] }<span onClick={() => { 따봉변경(따봉 + 1) }}>👍🏻</span> {따봉} </h4>
-        <p>6월 2일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{ 글제목[1] }</h4>
-        <p>6월 2일 발행</p>
-      </div>
-      <div className='list'>
-        <h4 onClick={()=>{ setModal(!modal) }}>{ 글제목[2] }</h4>
-        <p>6월 2일 발행</p>
-      </div> */}
 
         {
           글제목.map(function(a, i){
             return (
               <div className='list' key={i}>
-                <h4 >{ a } <span onClick={()=>{
+                <h4 onClick={()=>{setModal(true); setTitle(i)}}>{ 글제목[i] } <span onClick={(e)=>{
+                  e.stopPropagation();
+                  let copy = [...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy)
                 }}>👍🏻</span> { 따봉[i] } </h4>
                 <p>6월 2일 발행</p>
               </div>)
           })
         }
-
-
-
-
-
-        { modal == true ? <Modal /> : null }
+        <input onChange={(e)=>{ 입력값변경(e.target.value); console.log(입력값)}}/>
+        <button onClick={(e) => 글제목변경(글제목.push(e.target.value))}>글발행</button>
+        { modal == true ? <Modal 글제목={글제목} 글제목변경={글제목변경} setModal={setModal} title={title} setTitle={setTitle}/> : null }
     </div>
   );
 }
 
-function Modal(){
+
+function Modal(props){
   return (
     <div className='modal'>
-      <h4>제목</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
-  </div>
-  )
-}
-
-
+      <button onClick={() => {
+        let copy = [...props.글제목];
+        copy[0] = '여자 코트 추천'
+        props.글제목변경(copy);
+      }}>수정버튼</button>
+    </div>
+  )}
+    
+    
 
 export default App;
